ba4e470c0b87b0a2696d1d84b56d59787205de87
AtocM <atoc.mural@gmail.com>
2014-09-11 03:36:49 +0300

    FreshStart
    Opened New Project and Repository

---
+++ StockApplication/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
+
+namespace StockApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Panel topBarr = new Panel();
+
+        Panel topBarrr = new Panel();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Label nLabel = new Label();
+
+            nLabel.Content = "sdgsd";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
